f5b304e5530cffba29cafdfcd93b9721c5e51ae2
koheleth <coaleth@gmail.com>
2022-08-06 17:20:08 -0300

    Solution 2 of questions

--- lib/main.dart
+++ lib/main.dart
@@ -7,59 +7,61 @@ main() => runApp(PerguntaApp());
 
 class _PerguntaAppState extends State<PerguntaApp> {
   var _perguntaSelecionada = 0;
-  int qtdaPerguntas = 0;
+
+  final _perguntas = const [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
+    },
+    {
+      'texto': 'Qual é o seu tipo de filme?',
+      'respostas': ['Terror', 'Ação', 'Aventura', 'Comédia'],
+    },
+    {
+      'texto': 'Qual é o seu tipo de série?',
+      'respostas': ['Comédia', 'sitcom', 'mistério', 'policial'],
+    },
+    {
+      'texto': 'Qual é sua comida favorita?',
+      'respostas': ['Massas', 'sopas', 'doces', 'salgados'],
+    }
+  ];
 
   int _responder() {
     setState(() {
-      if (_perguntaSelecionada < qtdaPerguntas - 1) {
-        _perguntaSelecionada++;
-      } else {
-        _perguntaSelecionada = 0;
-      }
+      _perguntaSelecionada++;
     });
     print('Valor é $_perguntaSelecionada');
     return _perguntaSelecionada;
   }
 
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
+  }
+
   @override
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> perguntas = [
-      {
-        'texto': 'Qual é a sua cor favorita?',
-        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco'],
-      },
-      {
-        'texto': 'Qual é o seu animal favorito?',
-        'respostas': ['Coelho', 'Cobra', 'Elefante', 'Leão'],
-      },
-      {
-        'texto': 'Qual é o seu tipo de filme?',
-        'respostas': ['Terror', 'Ação', 'Aventura', 'Comédia'],
-      },
-      {
-        'texto': 'Qual é o seu tipo de série?',
-        'respostas': ['Comédia', 'sitcom', 'mistério', 'policial'],
-      },
-      {
-        'texto': 'Qual é sua comida favorita?',
-        'respostas': ['Massas', 'sopas', 'doces', 'salgados'],
-      }
-    ];
-    List<String> respostas =
-        perguntas[_perguntaSelecionada].cast()['respostas'];
-    qtdaPerguntas = perguntas.length;
+    List<String> respostas = temPerguntaSelecionada
+        ? _perguntas[_perguntaSelecionada].cast()['respostas']
+        : [];
 
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Column(
-          children: [
-            Questao(perguntas[_perguntaSelecionada]['texto'].toString()),
-            ...respostas.map((t) => Resposta(t, _responder)).toList(),
-          ],
-        ),
+        body: temPerguntaSelecionada
+            ? Column(
+                children: <Widget>[
+                  Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
+                  ...respostas.map((t) => Resposta(t, _responder)).toList(),
+                ],
+              )
+            : null,
       ),
     );
   }
